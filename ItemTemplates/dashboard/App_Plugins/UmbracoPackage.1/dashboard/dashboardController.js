e32ff7656021ff6b866bd89f81cb167bac7efa58
Kevin Jump <kevin@thejumps.co.uk>
2020-06-06 15:25:00 +0100

    rename dashboard item folder

---
+++ ItemTemplates/dashboard/App_Plugins/UmbracoPackage.1/dashboard/dashboardController.js
@@ -0,0 +1,22 @@
+/**
+ * @ngdoc 
+ * 
+ * @name: UmbracoPackage.1 dashboard
+ * @description: controls a dashboard inside umbraco
+ * 
+ */
+
+(function () {
+
+    'use strict';
+
+    function dashboardController($scope) {
+
+        var vm = this;
+
+    }
+
+    angular.module('umbraco')
+        .controller('umbracopackage._1.dashboardController', dashboardController);
+
+})();
