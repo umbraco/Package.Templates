08c2972142798568ae86cdc4526f65f9b9782913
Kevin Jump <kevin@jumoo.co.uk>
2020-11-19 14:06:04 +0000

    update naming - cases etc.

--- ItemTemplates/UIExamples/App_Plugins/UIExamples/Simple/simple.controller.js
+++ ItemTemplates/UIExamples/App_Plugins/UIExamples/Simple/simple.controller.js
@@ -2,14 +2,37 @@
 (function () {
     'use strict';
 
-    function simple($scope) {
+    function simple($scope,
+        localizationService,
+        overlayService) {
 
         var vm = this;
+        vm.doStuff = doStuff;
 
-        vm.doStuff = function () {
-            alert("hello world");
+        ///
+
+        function doStuff () {
+            openOverlay();
         };
 
+        function openOverlay() {
+
+            localizationService.localizeMany(["exampleOverlayTitle", "exampleOverlayMessage"])
+                .then(function (values) {
+
+                    var overlay = {
+                        title: values[0],
+                        content: values[1],
+                        disableBackdropClick: true,
+                        disableEscKey: true,
+                        submit: function () {
+                            overlayService.close();
+                        }
+                    };
+
+                    overlayService.confirmDelete(overlay);
+                });
+        }
     };
 
     angular.module('umbraco')
---
+++ ItemTemplates/UIExamples/App_Plugins/UIExamples/example.section.controller.js
@@ -0,0 +1,39 @@
+﻿
+(function () {
+    'use strict';
+
+    function exampleSection($scope) {
+
+        var vm = this;
+
+        vm.page = {
+            title: 'Examples',
+            description: 'UI Examples',
+            navigation: [
+                {
+                    'name': 'Icons',
+                    'alias': 'icons',
+                    'icon': 'icon-cupcake',
+                    'view': Umbraco.Sys.ServerVariables.umbracoSettings.appPluginsPath + '/uiexamples/icons.html',
+                },
+                {
+                    'name': 'basic',
+                    'alias': 'basic',
+                    'icon': 'icon-sprout color-green',
+                    'view': Umbraco.Sys.ServerVariables.umbracoSettings.appPluginsPath + '/uiexamples/basic.html',
+                },
+                {
+                    'name': 'simple',
+                    'alias': 'simple',
+                    'icon': 'icon-sprout color-green',
+                    'view': Umbraco.Sys.ServerVariables.umbracoSettings.appPluginsPath + '/uiexamples/Simple/simple.html',
+                    'active': true
+                }
+            ]
+        }
+
+    };
+
+    angular.module('umbraco')
+        .controller('exampleSectionController', exampleSection);
+})();
