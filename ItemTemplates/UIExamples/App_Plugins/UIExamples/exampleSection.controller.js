beea497a07f3d172ae9b3d84dd142ab8c1655de9
Kevin Jump <kevin@jumoo.co.uk>
2020-11-19 11:25:41 +0000

    UI Examples item.

---
+++ ItemTemplates/UIExamples/App_Plugins/UIExamples/exampleSection.controller.js
@@ -0,0 +1,33 @@
+﻿
+(function () {
+    'use strict';
+
+    function exampleSection($scope) {
+
+        var vm = this;
+
+        vm.page = {
+            title: 'Examples',
+            description: 'UI Examples',
+            navigation: [
+                {
+                    'name': 'Icons',
+                    'alias': 'icons',
+                    'icon': 'icon-cupcake',
+                    'view': Umbraco.Sys.ServerVariables.umbracoSettings.appPluginsPath + '/uiexamples/icons.html',
+                    'active': true
+                },
+                {
+                    'name': 'basic',
+                    'alias': 'basic',
+                    'icon': 'icon-sprout color-green',
+                    'view': Umbraco.Sys.ServerVariables.umbracoSettings.appPluginsPath + '/uiexamples/basic.html',
+                }
+            ]
+        }
+
+    };
+
+    angular.module('umbraco')
+        .controller('exampleSectionController', exampleSection);
+})();
