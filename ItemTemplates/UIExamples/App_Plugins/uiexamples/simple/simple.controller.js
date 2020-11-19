a8af372371341a12ff24e2d347a201e205c0cec6
Richard Ockerby <richard.ockerby@drummondcentral.co.uk>
2020-11-19 15:14:39 +0000

    Add grouped buttons, lang files and rejig the layout for the buttons page

--- ItemTemplates/UIExamples/App_Plugins/uiexamples/simple/simple.controller.js
+++ ItemTemplates/UIExamples/App_Plugins/uiexamples/simple/simple.controller.js
@@ -4,10 +4,48 @@
 
     function simple($scope,
         localizationService,
-        overlayService) {
+        overlayService,
+        exampleResource) {
 
         var vm = this;
         vm.doStuff = doStuff;
+        vm.linkAway = exampleResource.linkAway;
+
+        /// umb-button-group ///
+
+        vm.buttonGroup = {
+            defaultButton: {
+                labelKey: "example_groupedButton_default",
+                hotKey: "ctrl+d",
+                hotKeyWhenHidden: true,
+                buttonStyle: 'success',
+                handler: function () {
+                    // do magic here
+                    alert("you clicked the default button");
+                }
+            },
+            subButtons: [
+                {
+                    labelKey: "example_groupedButton_subButtonA",
+                    hotKey: "ctrl+a",
+                    hotKeyWhenHidden: true,
+                    handler: function () {
+                        // do magic here
+                        alert("you clicked a sub button A");
+                    }
+                },
+                {
+                    labelKey: "example_groupedButton_subButtonB",
+                    hotKey: "ctrl+b",
+                    hotKeyWhenHidden: true,
+                buttonStyle: 'success',
+                    handler: function () {
+                        // do magic here
+                        alert("you clicked a sub button B");
+                    }
+                }
+            ]
+        };
 
         ///
 
