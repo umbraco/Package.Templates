cee06e6062d058bb8cdbcc9b1c110d1965a0da99
David Brendel <info@davidbrendel.de>
2020-11-28 15:31:37 +0100

    Updated tabs handling
    Correctly init tabs to show them when content app is selected
    Fixed issue with umb-code-snippet
    Added transaltation for api buttons and moved them to umb-box header

--- ItemTemplates/UIExamples/App_Plugins/uiexamples/tabs/tabs.controller.js
+++ ItemTemplates/UIExamples/App_Plugins/uiexamples/tabs/tabs.controller.js
@@ -1,23 +1,13 @@
 ﻿(function () {
     'use strict';
 
-    function tabsController($scope, eventsService) {
+    function tabsController($scope, $timeout, eventsService) {
 
         var vm = this;
 
+        vm.loading = true;
+        vm.tabs = [];
         vm.changeTab = changeTab;
-
-        vm.tabs = [
-            {
-                "alias": "tabOne",
-                "label": "HTML",
-                "active": true
-            },
-            {
-                "alias": "tabTwo",
-                "label": "JavaScript"
-            }
-        ];
 
         function changeTab(selectedTab) {
             vm.tabs.forEach(function (tab) {
@@ -26,11 +16,27 @@
             selectedTab.active = true;
         };
 
-        eventsService.on("app.tabChange", function (args) {
-            console.log("args", args);
+        eventsService.on("app.tabChange", function (event, args) {
+            console.log('Tab switch called', event, args);
+            $timeout(function () {
+                if (args.alias === 'tabs') {
+                    console.log('Tab app active');
+                    vm.tabs = [
+                        {
+                            "alias": "tabOne",
+                            "label": "HTML",
+                            "active": true
+                        },
+                        {
+                            "alias": "tabTwo",
+                            "label": "JavaScript"
+                        }
+                    ];
+                    vm.loading = false;
+                }
+            });
         });
     };
 
-    angular.module('umbraco')
-        .controller('tabsController', tabsController);
+    angular.module('umbraco').controller('tabsController', tabsController);
 })();
