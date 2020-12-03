a6e73123a9b8e1da45435bbfe5cd2e58e55ccb9d
David Brendel <info@davidbrendel.de>
2020-12-03 12:33:20 +0100

    Added TreeController Item Template

---
+++ ItemTemplates/TreeController/Trees/_treeTitle_TreeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Formatting;
+using System.Web.Http.ModelBinding;
+using Umbraco.Web.Models.ContentEditing;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Trees;
+using Umbraco.Web.WebApi.Filters;
+
+namespace UmbracoPackage._1.Trees
+{
+    [Tree("#sectionAlias#", "#treeAlias#", TreeTitle = "#treeTitle#")]
+#if (searchable)
+    public class #treeTitle#TreeController : TreeController, ISearchableTree
+#else
+    public class #treeTitle#TreeController : TreeController
+#endif
+    {
+        protected override MenuItemCollection GetMenuForNode(string id, [ModelBinder(typeof(HttpQueryStringModelBinder))] FormDataCollection queryStrings)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override TreeNodeCollection GetTreeNodes(string id, [ModelBinder(typeof(HttpQueryStringModelBinder))] FormDataCollection queryStrings)
+        {
+            throw new NotImplementedException();
+        }
+
+#if (searchable)
+        public IEnumerable<SearchResultEntity> Search(string query, int pageSize, long pageIndex, out long totalFound, string searchFrom = null)
+        {
+            throw new NotImplementedException();
+        }
+#endif
+    }
+}
