8cf3fe74ebc12c01e06f5706f3325b42236f8de6
Kevin Jump <kevin@jumoo.co.uk>
2020-12-14 16:08:51 +0000

    icon - colour picker overlay.

---
+++ ItemTemplates/UIExamples/App_Plugins/uiexamples/icons/iconoverlay.controller.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    'use strict';
+
+    function overlayController() {
+
+        var vm = this;
+
+        vm.colors = [
+            { name: "Black", value: "color-black", default: true },
+            { name: "Blue Grey", value: "color-blue-grey" },
+            { name: "Grey", value: "color-grey" },
+            { name: "Brown", value: "color-brown" },
+            { name: "Blue", value: "color-blue" },
+            { name: "Light Blue", value: "color-light-blue" },
+            { name: "Indigo", value: "color-indigo" },
+            { name: "Purple", value: "color-purple" },
+            { name: "Deep Purple", value: "color-deep-purple" },
+            { name: "Cyan", value: "color-cyan" },
+            { name: "Green", value: "color-green" },
+            { name: "Light Green", value: "color-light-green" },
+            { name: "Lime", value: "color-lime" },
+            { name: "Yellow", value: "color-yellow" },
+            { name: "Amber", value: "color-amber" },
+            { name: "Orange", value: "color-orange" },
+            { name: "Deep Orange", value: "color-deep-orange" },
+            { name: "Red", value: "color-red" },
+            { name: "Pink", value: "color-pink" }
+        ];
+
+    }
+
+    angular.module('umbraco')
+        .controller('exampleSectionIconsOverlayController', overlayController);
+})();
--- ItemTemplates/UIExamples/App_Plugins/uiexamples/icons/icons.controller.js
+++ ItemTemplates/UIExamples/App_Plugins/uiexamples/icons/icons.controller.js
@@ -1,10 +1,12 @@
 (function () {
     'use strict';
 
-    function exampleSectionIconsController(exampleResource, iconHelper) {
+    function exampleSectionIconsController(exampleResource, iconHelper, overlayService) {
 
         var vm = this;
         vm.linkAway = exampleResource.linkAway;
+
+        vm.openIconOverlay = openIconOverlay;
 
         function init() {
 
@@ -32,6 +34,23 @@
                     vm.icons = legacyIcons.concat(vm.icons);
                 }
             });
+        }
+
+        /////////
+
+        function openIconOverlay(icon) {
+
+            var options = {
+                view: Umbraco.Sys.ServerVariables.umbracoSettings.appPluginsPath + '/uiexamples/icons/iconOverlay.html',
+                title: icon.name,
+                content: icon.name,
+                icon: icon,
+                hideSubmitButton: true,
+                close: function () {
+                    overlayService.close();
+                }
+            }
+            overlayService.open(options);
         }
 
         init();
